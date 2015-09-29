afcfa93c7384edfc95b4fa1892339f233e8f8edc
kaiyusung <edward.kaiyu@gmail.com>
2015-09-29 02:27:27 -0700

    3rd commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :dashboards
+
   get "/dropbox",  :controller => "dropbox", :action => "index"
   get "/dropbox/auth",  :controller => "dropbox", :action => "show"
   get "/dropbox/callback",  :controller => "dropbox", :action => "create"
